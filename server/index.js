58e5df89a0e61a93949336958d4035f34e54c558
Ansh Maheshwari <anshmaheshwari82@gmail.com>
2025-05-04 06:10:46 +0530

    fixed issues

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express=require('express');
+const app=express();
+const cors=require('cors')
+require('dotenv').config();
+
+var cookieParser = require('cookie-parser');
+
+//------------miidleware-----------
+const fileUpload=require('express-fileupload');
+
+app.use(fileUpload(
+    {
+        useTempFiles:true,
+        tempFileDir:'/tmp/'
+    }
+    ));
+
+
+
+    app.use(express.json());
+    app.use(cookieParser());
+    
+    
+    
+    app.use(cors({
+      origin: 'http://localhost:3000',
+    //   origin: 'https://community-cares-9y35.vercel.app',
+      optionsSuccessStatus: 200,
+      credentials: true 
+    }));
+
+    const router=require('./route/routes.jsx')
+    
+    app.use('/api/v1',router)
+
+
+//--------------db connect-----------------
+const dbConnect=require('./config/database.jsx');
+dbConnect();
+
+const cloudinary=require('./middlewares/cloudinary.jsx');
+cloudinary.cloudinaryConnect();
+
+//-------------server started---------------------
+
+app.listen(process.env.PORT,()=>{
+    // console.log(`server started at port ${process.env.PORT}`)
+})
+
+
+module.exports= app;
